9e34b426dba19224a908c2a047ed2a22eeea8338
Skyler <sreedgets@outlook.com>
2021-12-07 21:55:01 -0600

    Building static layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <main className="tenzies--container">
+            <div className="tenzies--header-wrapper">
+                <h1>Tenzies</h1>
+                <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            </div>
+            <div className="tenzies--dice-wrapper">
+                <div className="tenzies--die">0</div>
+                <div className="tenzies--die">1</div>
+                <div className="tenzies--die">2</div>
+                <div className="tenzies--die">3</div>
+                <div className="tenzies--die">4</div>
+                <div className="tenzies--die">5</div>
+                <div className="tenzies--die">6</div>
+                <div className="tenzies--die">7</div>
+                <div className="tenzies--die">8</div>
+                <div className="tenzies--die">9</div>
+            </div>
+            <div className="tenzies--button-wrapper">
+                <button>Roll</button>
+            </div>
+        </main>
+    )
 }
-
-export default App;
